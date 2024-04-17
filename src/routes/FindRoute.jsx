f017d3eab17b5e1b49d8dea30475fb4fb9cf0baf
Hoda96 <farahani.h40@gmail.com>
2024-04-17 11:10:53 +0330

    upload geojson by user

--- src/routes/FindRoute.jsx
+++ src/routes/FindRoute.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect, useState, useContext } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "../App.css";
-import MapComponent from "../layouts/MapComponent";
 import MapContext from "../context/MapContext";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -22,7 +21,6 @@ export default function FindRoute() {
     start: null,
     destination: null,
   });
-
 
   function handlePoint(type) {
     if (markers[type]) {
@@ -55,57 +53,54 @@ export default function FindRoute() {
     mapRef.current.on("click", add_marker);
   }
 
-  async function  handleFindRoute() {
-    
-    
+  async function handleFindRoute() {
     const allCoordsAreNotSet =
-    coordinates.startLng === null ||
-    coordinates.startLat === null ||
-    coordinates.destinationLng === null ||
-    coordinates.destinationLat === null;
-    
+      coordinates.startLng === null ||
+      coordinates.startLat === null ||
+      coordinates.destinationLng === null ||
+      coordinates.destinationLat === null;
+
     if (allCoordsAreNotSet) {
       console.log("Start or destination coordinates are not set");
       return;
     }
-    
-    const geometry= await getRoute();
+
+    const geometry = await getRoute();
     if (!geometry) {
       console.log("Geometry is null");
       return;
-    };
+    }
 
     const routeSource = mapRef.current.getSource("route");
-      // Check if the route source already exists
-      if (routeSource) {
-        // Update the route source data with the new coordinates
-        routeSource.setData({
+    // Check if the route source already exists
+    if (routeSource) {
+      // Update the route source data with the new coordinates
+      routeSource.setData({
+        type: "Feature",
+        properties: {},
+        geometry: geometry,
+      });
+    } else {
+      // If the route source does not exist, add it
+      mapRef.current.addSource("route", {
+        type: "geojson",
+        data: {
           type: "Feature",
-          properties: {},
           geometry: geometry,
-  
-        });
-      } else {
-        // If the route source does not exist, add it
-        mapRef.current.addSource("route", {
-          type: "geojson",
-          data: {
-            type: "Feature",
-            geometry: geometry,
-          },
-        });
+        },
+      });
 
-        // Add the route layer
-        mapRef.current.addLayer({
-          id: "route",
-          type: "line",
-          source: "route",
-          paint: {
-            "line-color": "#eb4034", // Color of the route
-            "line-width": 4, // Width of the route
-          },
-        });
-      }
+      // Add the route layer
+      mapRef.current.addLayer({
+        id: "route",
+        type: "line",
+        source: "route",
+        paint: {
+          "line-color": "#eb4034", // Color of the route
+          "line-width": 4, // Width of the route
+        },
+      });
+    }
   }
 
   const getRoute = async () => {
@@ -128,7 +123,6 @@ export default function FindRoute() {
 
       const geometry = data.routes[0].geometry;
 
-
       console.log("geometryyyy", geometry);
       // setGeometry(geometry);
       return geometry;
@@ -143,7 +137,6 @@ export default function FindRoute() {
         <div className="groupBtn">
           <button
             className="btn"
-            id="point"
             onClick={() => handlePoint("start")}
             style={{
               backgroundColor: point.start ? "#ccc1c0" : "",
@@ -154,7 +147,6 @@ export default function FindRoute() {
           </button>
           <button
             className="btn"
-            id="point"
             onClick={() => handlePoint("destination")}
             style={{
               backgroundColor: point.destination ? "#ccc1c0" : "",
--- src/routes/Geofence.jsx
+++ src/routes/Geofence.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import MapContext from "../context/MapContext";
 
 export default function Geofence() {
+  const mapRef = useContext(MapContext);
+
+  const [selectedFile, setSelectedFile] = useState();
+
+  const handleFileChange = (event) => {
+    console.log("file uploaded:", event.target.files);
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!selectedFile) {
+      console.log("Please select a file first");
+      return;
+    }
+    // Create a new FileReader object
+    let reader = new FileReader();
+    console.log("reader:", reader);
+
+    // reader.onprogress = ()=> alert("file is prossesing")
+    // Setup the callback event to run when file reading is complete
+    reader.onloadend = (e) => {
+      console.log("file", e.target.result);
+      const geojson = e.target.result;
+      return geojson;
+    };
+    // Read the file's content
+    reader.readAsText(selectedFile);
+
+    // Here you can add the code to upload 'selectedFile' to a server
+    console.log(`File ${selectedFile.name} is ready to be uploaded.`);
+  };
+
   return (
     <div className="container">
       <div className="sidebar">
-
-Geofence
-
+        <div className="groupBtn">
+          <label className="title">Upload geojson file</label>
+          <input
+            type="file"
+            onChange={handleFileChange}
+            className="uploadBtn"
+          />
+          <button onClick={handleUpload} className="btn">
+            Upload
+          </button>
+        </div>
       </div>
     </div>
   );
